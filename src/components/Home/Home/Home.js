825a7b0db510b34a76027045c6e29ae2beed1521
Badhan112 <badhancsesastc112@gmail.com>
2021-05-29 15:53:44 +0600

    ticket booking form

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import TicketBookingForm from '../TicketBookingForm/TicketBookingForm';
+import './Home.css';
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <main className='light-bg'>
+            <TicketBookingForm />
+        </main>
     );
 };
 
---
+++ src/components/Home/TicketBookingForm/TicketBookingForm.js
@@ -0,0 +1,81 @@
+import React from 'react';
+
+const TicketBookingForm = () => {
+
+    const handleSubmit = e => {
+        e.preventDefault();
+    }
+    
+    return (
+        <section className='container'>
+            <form onSubmit={handleSubmit} className='row p-2 justify-content-center align-items-center'>
+                <h3>Booking Flight</h3>
+                <div className='col-lg-3 col-md-4 col-sm-12'>
+                    <div class="mb-3">
+                        <label for="flyingFrom" class="form-label">Flying From</label>
+                        <input type="text" class="form-control" id="flyingFrom" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="flyingTo" class="form-label">Flying To</label>
+                        <input type="text" class="form-control" id="flyingto" />
+                    </div>
+                </div>
+
+                <div className='col-lg-3 col-md-4 col-sm-12'>
+                    <div class="mb-3">
+                        <label for="departure" class="form-label">Departure</label>
+                        <input type="datetime-local" class="form-control" id="departure" />
+                    </div>
+                    <div class="mb-3">
+                        <label for="return" class="form-label">Return</label>
+                        <input type="datetime-local" class="form-control" id="return" />
+                    </div>
+                </div>
+
+                <div className='col-lg-3 col-md-4 col-sm-12'>
+                    <div class="mb-3">
+                        <label for="firstClass" class="form-label">First Class</label>
+                        <div className='d-flex'>
+                            <input type="number" class="form-control" id="firstClass" readOnly style={{ backgroundColor: 'white' }} />
+                            <button className="btn btn-secondary mx-2">+</button>
+                            <button className="btn btn-secondary">-</button>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="economy" class="form-label">Economy</label>
+                        <div className="d-flex">
+                            <input type="number" class="form-control" id="economy" readOnly style={{ backgroundColor: 'white' }} />
+                            <button className="btn btn-secondary mx-2">+</button>
+                            <button className="btn btn-secondary">-</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='col-lg-3 col-md-4 col-sm-12'>
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>Sub-Total</td>
+                                <td>0</td>
+                            </tr>
+                            <tr>
+                                <td>Charge</td>
+                                <td>10% VAT</td>
+                            </tr>
+                            <tr>
+                                <td>Total</td>
+                                <td>0</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+
+                <div className='col-12 text-center'>
+                    <button type="submit" class="btn btn-primary w-50">Submit</button>
+                </div>
+            </form>
+        </section>
+    );
+};
+
+export default TicketBookingForm;
