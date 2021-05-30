f5ff4b1057ad4ca79ccfb9cfcbf731d6c15e2a8f
Badhan112 <badhancsesastc112@gmail.com>
2021-05-30 14:16:35 +0600

    form functionality added

--- src/components/About/About/About.js
+++ src/components/About/About/About.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            About
-        </div>
+        <main>
+            <section className='container'>
+
+            </section>
+        </main>
     );
 };
 
